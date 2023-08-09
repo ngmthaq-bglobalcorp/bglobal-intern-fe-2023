ab3398f67d8558c89904d980d83e3a583aadfde6
pinkypea <daokien2303@gmail.com>
2023-08-09 15:09:38 +0700

    create History view

--- src/views/UserViews/History/HistoryView.vue
+++ src/views/UserViews/History/HistoryView.vue
@@ -1,22 +1,23 @@
 <template>
-    <BaseLayout><JobCard></JobCard></BaseLayout>
-  </template>
-  
-  <script setup lang="ts">
-  import { BaseComponent, defineClassComponent } from "@/plugins/component.plugin";
-  
-  import JobCard from "@/components/UserComponents/JobCard/JobCard.vue";
-  import BaseLayout from "@/layouts/BaseLayout.vue";
-  const app = defineClassComponent(
-    class Component extends BaseComponent {
-      public constructor() {
-        super();
-      }
-    },
-  );
-  </script>
-  
-  <style scoped lang="scss">
-  @import "@/assets/scss/modules";
-  </style>
-  
+  <BaseLayout>
+    <JobCard />
+  </BaseLayout>
+</template>
+
+<script setup lang="ts">
+import { BaseComponent, defineClassComponent } from "@/plugins/component.plugin";
+
+import JobCard from "@/components/UserComponents/JobCard/JobCard.vue";
+import BaseLayout from "@/layouts/BaseLayout.vue";
+const app = defineClassComponent(
+  class Component extends BaseComponent {
+    public constructor() {
+      super();
+    }
+  },
+);
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/modules";
+</style>
